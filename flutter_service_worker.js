009a559a0bc36eda1878f36b6b9fc6c6275ebd5e
Onat Çipli <onatcipli@outlook.com>
2021-04-04 11:22:43 +0300

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "4b6db237b3514a88107a422469adfb0f",
-"index.html": "70b9e343bb1001f54c126d6a08c76071",
-"/": "70b9e343bb1001f54c126d6a08c76071",
-"main.dart.js": "b54e4cd98c6def9ebbb472b2ba559f35",
+"index.html": "8613454d209bdd50512bc316ac734c78",
+"/": "8613454d209bdd50512bc316ac734c78",
+"main.dart.js": "af5adcfcf9423445aa19ab281da0e8f6",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
