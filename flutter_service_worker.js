d14dbdd62408a03d9a5e78054a29ca71f51306e8
Onat Çipli <onatcipli@outlook.com>
2021-04-04 10:59:07 +0300

    updates

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "4b6db237b3514a88107a422469adfb0f",
-"index.html": "c115bd98226f106e1064acec3647ef8e",
-"/": "c115bd98226f106e1064acec3647ef8e",
-"main.dart.js": "9d7c37cccbdf697f524b8b0af7f5ff08",
+"index.html": "70b9e343bb1001f54c126d6a08c76071",
+"/": "70b9e343bb1001f54c126d6a08c76071",
+"main.dart.js": "b54e4cd98c6def9ebbb472b2ba559f35",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
